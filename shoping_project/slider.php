738e8ed2a3b1e12c589603f3d7e19e4845b79062
Nitinkumar-7 <nitinkandwal0@gmail.com>
2023-04-11 11:32:28 +0530

    some changes

--- shoping_project/slider.php
+++ shoping_project/slider.php
@@ -17,59 +17,76 @@
  <!-- slider html code  -->
  <div class="slideshow-container">
 
-<!-- images are added for the slider  -->
 <div class="mySlides fade">
-  <img class="slideimg" src="images/banner-1.webp">
+  
+  <img src="images/banner-5.webp" style="width:100%">
+  
 </div>
 
 <div class="mySlides fade">
-  <img class="slideimg" src="images/banner-2.webp">
+
+  <img src="images/banner-4.webp" style="width:100%">
+  
 </div>
 
 <div class="mySlides fade">
-  <img class="slideimg" src="images/banner-3.webp">
+  
+  <img src="images/banner-3.webp" style="width:100%">
+ 
 </div>
 
 <div class="mySlides fade">
-  <img class="slideimg" src="images/banner-4.webp">
+  
+  <img src="images/banner-2.webp" style="width:100%">
+  
 </div>
 
 <div class="mySlides fade">
-  <img class="slideimg" src="images/banner-5.webp">
+  
+  <img src="images/banner-1.webp" style="width:100%">
+  
 </div>
-</div>
-
-<br>
 
 <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
 <a class="next" onclick="plusSlides(1)">&#10095;</a>
-<!-- nevigation dots  -->
-<div style="text-align:center">
-<span class="dot"></span>
-<span class="dot"></span>
-<span class="dot"></span>
-<span class="dot"></span>
-<span class="dot"></span>
 
 </div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
 <script>
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+showSlides();
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function plusSlides(n) {
+  slideIndex += n;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (slideIndex > slides.length) {slideIndex = 1}
+  if (slideIndex < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
@@ -80,6 +97,25 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+function currentSlide(n) {
+  slideIndex = n;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (slideIndex > slides.length) {slideIndex = 1}
+  if (slideIndex < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
 </script>
 </body>
 </html>
+
+
+
+
